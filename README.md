b3338d84b2419f46c068b21b4ed739d140ddc0f5
Josh Espinosa <hauuguu@gmail.com>
2018-04-17 17:07:07 -0400

    Init

---
+++ README.md
@@ -0,0 +1,18 @@
+# discord-chat-relay
+
+Relay chat between Tera (Guild Chat only) & Discord
+
+This is based off of [Meishuu's Tera-Discord-Relay](https://github.com/meishuu/tera-discord-relay)
+
+Tera-strings may need updating in the future. Latest one I pulled was from [trini0n's fork](https://github.com/trini0n/tera-discord-relay/blob/ccb4af6a6fdeefb7480068d4ba5fb44ab0e5fbc4/tera/app/discord/node_modules/tera-strings/strings.json)
+
+## REQUIREMENTS
+
+* This requires Caali's proxy via https://discord.gg/maqBmJV
+
+## INSTALLATION
+
+* Download or clone this repo and then run `npm install`
+* Fill out the information in config/sample-config.json and rename to config.json
+* Place folder in tera-proxy\bin\node_modules\
+* Launch proxy and then start Tera and enjoy
---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const DiscordHandler = require('./lib/discordHandler.js');
+const TeraHandler = require('./lib/teraHandler.js');
+const U = require('./lib/util');
+
+class DiscordChatRelay {
+  constructor(dispatch) {
+    /* CONFIG FILE */
+    const config = JSON.parse(
+      fs.readFileSync(path.join(__dirname, '/config/config.json'), 'utf8')
+    );
+
+    if (!config) {
+      console.error('no config file found');
+      process.exit(1);
+    }
+
+    /* INIT Discord & Tera listeners*/
+    const discordHandler = new DiscordHandler(dispatch, config);
+    const teraHandler = new TeraHandler(dispatch, config);
+
+    this.destructor = () => {
+      teraHandler.setLoginStatus(false);
+    };
+
+    // ping-pong
+    dispatch.hook('S_PING', 1, () => {
+      dispatch.toServer('C_PONG', 1);
+    });
+  }
+}
+
+module.exports = DiscordChatRelay;
