2e90308733ed6e3bd25368e1a9730513c614fe1f
Josh Espinosa <hauuguu@gmail.com>
2018-04-18 02:18:11 -0400

    Updated for Awakened patch

--- index.js
+++ index.js
@@ -8,27 +8,20 @@ const U = require('./lib/util');
 class DiscordChatRelay {
   constructor(dispatch) {
     /* CONFIG FILE */
-    const config = JSON.parse(
-      fs.readFileSync(path.join(__dirname, '/config/config.json'), 'utf8')
-    );
+    const config = JSON.parse(fs.readFileSync(path.join(__dirname, '/config/config.json'), 'utf8'));
 
     if (!config) {
       console.error('no config file found');
       process.exit(1);
     }
 
-    /* INIT Discord & Tera listeners*/
+    /* INIT Discord & Tera listeners */
     const discordHandler = new DiscordHandler(dispatch, config);
     const teraHandler = new TeraHandler(dispatch, config);
 
     this.destructor = () => {
       teraHandler.setLoginStatus(false);
     };
-
-    // ping-pong
-    dispatch.hook('S_PING', 1, () => {
-      dispatch.toServer('C_PONG', 1);
-    });
   }
 }
 
